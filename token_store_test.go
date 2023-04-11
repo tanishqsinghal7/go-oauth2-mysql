a1f6ebf1139591631155a849cd45e4ed22b59f79
tanishq.singhal <tanishq.singhal@ril.com>
2023-04-11 10:41:48 +0530

    Fix error for import in token store

--- token_store_test.go
+++ token_store_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	. "github.com/passwind/go-oauth2-mysql/v4"
+	. "github.com/tanishqsinghal7/go-oauth2-mysql/v4"
 )
 
 func generateTokenTableName() string {
